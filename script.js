8bee300f550d411075cffdf0e4cfae57d8b0aa29
Katrina Moody <137461461+kmmoody212@users.noreply.github.com>
2024-10-02 12:50:31 -0400

    Added size chart img; Created, designed, and made logic for size chart modal.

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-document.getElementById('reviewForm').addEventListener('submit', function(event) {
+document
+  .getElementById("reviewForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    
-    const reviewText = document.getElementById('review').value;
-    document.getElementById('feedbackMessage').innerText = `Your feedback: "${reviewText}" is always appreciated`;
-    document.getElementById('feedbackMessage').style.display = 'block';
-    
-    document.getElementById('review').style.display = 'none';
-    this.querySelector('input[type="submit"]').style.display = 'none';
+
+    const reviewText = document.getElementById("review").value;
+    document.getElementById(
+      "feedbackMessage"
+    ).innerText = `Your feedback: "${reviewText}" is always appreciated`;
+    document.getElementById("feedbackMessage").style.display = "block";
+
+    document.getElementById("review").style.display = "none";
+    this.querySelector('input[type="submit"]').style.display = "none";
   });
+
+// Modal logic for size chart
+
+const open = document.getElementById("size-chart-btn");
+const modalContainer = document.querySelector(".size-modal-container");
+const close = document.getElementById("close-modal");
+
+open.addEventListener("click", () => {
+  modalContainer.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
