20d560cb9f45f9b86a48363c02b30ed27da20a34
jacobmidd1996 <debacled1996@gmail.com>
2024-10-02 12:57:08 -0400

    shirt button update

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-<<<<<<< HEAD
 function changeImage(imagePath) {
-    document.getElementById('images').src = imagePath;
+  document.getElementById("images").src = imagePath;
 }
-=======
-document.getElementById('reviewForm').addEventListener('submit', function(event) {
+document
+  .getElementById("reviewForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    
-    const reviewText = document.getElementById('review').value;
-    document.getElementById('feedbackMessage').innerText = `Your feedback: "${reviewText}" is always appreciated`;
-    document.getElementById('feedbackMessage').style.display = 'block';
-    
-    document.getElementById('review').style.display = 'none';
-    this.querySelector('input[type="submit"]').style.display = 'none';
+
+    const reviewText = document.getElementById("review").value;
+    document.getElementById(
+      "feedbackMessage"
+    ).innerText = `Your feedback: "${reviewText}" is always appreciated`;
+    document.getElementById("feedbackMessage").style.display = "block";
+
+    document.getElementById("review").style.display = "none";
+    this.querySelector('input[type="submit"]').style.display = "none";
   });
->>>>>>> 321fe5971b8c36b3f018484c4c2a14741edd96cd
